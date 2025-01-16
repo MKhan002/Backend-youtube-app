1b557bff11245803c35220219f295a6687f2530a
MKhan002 <mk9901502@gmail.com>
2025-01-16 14:18:23 +0500

    all setup configured

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -14,7 +14,14 @@ const userSchema = new Schema(
       trim: true,
       index: true, // Optimized for searching
     },
-    fullname: {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullName: {
       type: String,
       required: true,
       trim: true,
@@ -51,7 +58,7 @@ const userSchema = new Schema(
 userSchema.pre("save", async function (next) {
   if(this.isModified("password")) return next();
 
-  this.password = bcrypt.hash(this.password, 10)
+  this.password = await bcrypt.hash(this.password, 10)
   next();
 })
 
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,23 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
+
 
 const userRouter = Router();
 
-userRouter.route("/register").post(registerUser)
+userRouter.route("/register").post(
+    upload.fields([
+        {
+            name: "avator",
+            maxCount: 1
+        },
+        {
+            name: "coverImage",
+            maxcount: 1
+        }
+    ]),
+    registerUser
+)
 
 //router.route("/login").post(resgisterUser);
 
